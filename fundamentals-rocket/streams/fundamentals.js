8691099004a5d37c5ad610fbf1b7248c3765c458
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-01 23:56:32 -0400

    fundamentals: learning about writing and transforming streams

--- fundamentals-rocket/streams/fundamentals.js
+++ fundamentals-rocket/streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
 class OneToHundredSteam extends Readable {
   index = 1;
@@ -18,4 +18,21 @@ class OneToHundredSteam extends Readable {
   }
 }
 
-new OneToHundredSteam().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString() * -1);
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredSteam()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
