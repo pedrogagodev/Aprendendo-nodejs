f284c246dca7fc0f86f025c5a2fbad93288e2d4b
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-08 00:44:52 -0400

    fundamentals: capturing query parameters

--- fundamentals-rocket/src/routes.js
+++ fundamentals-rocket/src/routes.js
@@ -9,6 +9,8 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
+      console.log(req.query);
+
       const users = database.select("users");
 
       return res.end(JSON.stringify(users));
@@ -42,7 +44,7 @@ export const routes = [
         email,
       });
 
-    return res.writeHead(204).end();
+      return res.writeHead(204).end();
     },
   },
   {
