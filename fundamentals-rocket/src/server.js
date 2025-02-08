8c71711fa868e8c26c719327f35346044ab160de
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-07 22:34:41 -0400

    fundamentals: separating application routes

--- fundamentals-rocket/src/server.js
+++ fundamentals-rocket/src/server.js
@@ -1,32 +1,20 @@
 import http from "node:http";
-import { randomUUID } from 'node:crypto'
-import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  const router = routes.find((req) => {
+    return req.method === method && req.path === url;
+  });
 
-    return res.end(JSON.stringify(users));
+  if (router) {
+    return router.handler(req, res);
   }
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
 
-    database.insert("users", user);
-
-    return res.writeHead(201).end();
-  }
   return res.writeHead(404).end();
 });
 
