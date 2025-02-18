7fa7a31b9cffde143cc7b33dbe148edb9de427d1
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-18 14:36:25 -0400

    mycontacts: add biomejs

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,9 +1,9 @@
-import { express } from 'express'
+import { express } from "express";
 
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("Hello, world!");
+	response.send("Hello, world!");
 });
 
 app.listen(3000, () => console.log("Server started in http://localhost:3000"));
