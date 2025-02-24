0888e974af2d7724026e137fb2df4c04a1864f0f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-24 19:42:16 -0400

    rocketAPI: creating POST method

--- 02-rocket-API-REST/src/routes/transactions.ts
+++ 02-rocket-API-REST/src/routes/transactions.ts
@@ -1,16 +1,26 @@
 import type { FastifyInstance } from "fastify";
 import { knex } from "../database";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transactions")
-      .insert({
-        id: crypto.randomUUID(),
-        title: "Test transaction",
-        amount: 1000,
-      })
-      .returning("*");
+  app.post("/", async (request, reply) => {
+    // { title, amount, type: credit or debit }
 
-    return transaction;
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, type, amount } = createTransactionSchema.parse(request.body);
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
   });
 }
