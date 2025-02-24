5cf5c882b76d74e0bf3d608361810770de26cf52
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-24 17:00:48 -0400

    rocketAPI: create a plugin with fastify

--- 02-rocket-API-REST/src/server.ts
+++ 02-rocket-API-REST/src/server.ts
@@ -2,20 +2,11 @@ import fastify from "fastify";
 import crypto from "node:crypto";
 import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transaction = await knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Test transaction",
-      amount: 1000,
-    })
-    .returning("*");
-
-  return transaction;
-});
+app.register(transactionsRoutes);
 
 app
   .listen({
